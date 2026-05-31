00912ba42d9437f200906f39b610882fe6f7960b
unknown <vermaaadityaff123@gmail.com>
2026-05-31 16:44:26 +0530

    feat: initial commit of internly backend and dashboard

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import InternTracker from './components/InternTracker';
+
+function App() {
+  return (
+    <div style={{ padding: '0px 0px 40px 0px' }}>
+      <InternTracker />
+    </div>
+  );
+}
+
+export default App;
